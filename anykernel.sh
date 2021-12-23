b9e3eb626d7eca9a21c61abd7f18d491bbb1292d
Omar Hamad <omartldr@gmail.com>
2021-12-23 21:39:47 +0200

    chips

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Predator Stormbreaker Kernel  by sohamsen  @ xda-developers
+kernel.string=Chips Kernel EAS by etahamad @ telegram/github
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=lavender
-supported.versions=9 - 12
+supported.versions=12
 supported.patchlevels=
 '; } # end properties
 
@@ -20,45 +20,12 @@ block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-
-
 ## AnyKernel boot install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-
-# init.tuna.rc
-
-# fstab.tuna
-
-# end ramdisk changes
-
 write_boot;
 ## end boot install
-
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
-
